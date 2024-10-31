1711f1afb3556d9d54bd5f6dcbf2d33d98b30fdd
devkobby24 <nanakobby2002@gmail.com>
2024-10-31 17:12:06 +0000

    Refactor Hero section button for improved styling and user experience

--- app/components/ui/Hero.js
+++ app/components/ui/Hero.js
@@ -11,18 +11,16 @@ export default function HeroSection() {
       />
       <div className="absolute inset-0 bg-black opacity-50" />
       <div className="relative container mx-auto px-4 h-full flex flex-col justify-center items-center text-center">
-        <h1 className="text-5xl font-bold mb-4 text-white brightness-150">
+        <h1 className="text-5xl font-bold mb-4 font-sans text-white brightness-150">
           Welcome to Glorious Assurance Ministries
         </h1>
         <p className="text-xl mb-8">
           Join us in worship, fellowship, and spiritual growth
         </p>
         <Link href="/contact">
-          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-              Worship With Us
-            </span>
+          <button className="px-8 py-2 h-14 rounded-full relative bg-gray-900 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
+            <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+            <span className="relative z-20">Worship with Us</span>
           </button>
         </Link>
       </div>
