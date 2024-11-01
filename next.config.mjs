86ffc83c32061a6505e328c82e3d0e7042e2e9bc
devkobby24 <nanakobby2002@gmail.com>
2024-11-01 08:49:47 +0000

    Configure image domains in next.config.mjs for external image support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['https://gloriousassuranceministires.vercel.app'], // Replace with your Vercel domain, e.g., 'your-project-name.vercel.app'
+    },
+  };
+  
+  module.exports = nextConfig;
+  
